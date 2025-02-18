546012348375d1c0f5b720acecff6b9fdb259312
Pavanagula-19 <Pavanagulla19@gmail.com>
2025-02-18 08:37:05 +0530

    static deploy fixed in cors setting

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,7 @@ MongoDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://presento-notes-app.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true,
   })
